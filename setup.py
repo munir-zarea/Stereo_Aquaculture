680ec8d44da04fafc3babce69b5e8f7be775b3a0
Munir Zarea <123416168+munir-zarea@users.noreply.github.com>
2025-08-26 11:43:06 -1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 package_name = 'mmlove_stereo_recorder'
 
 setup(
     name=package_name,
-    version='0.0.1',
-    packages=find_packages(),
+    version='0.1.0',
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/stereo.launch.py']),
-        ('share/' + package_name + '/config', ['config/stereo_params.yaml']),
+        ('share/' + package_name + '/launch', ['launch/sbs.launch.py']),
+        ('share/' + package_name + '/config', ['config/sbs_camera.yaml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Munir Zarea',
     maintainer_email='zaream@hawaii.edu',
-    description='MMLove stereo camera launcher & rosbag recording',
+    description='Stereo SBS camera recorder',
     license='Apache-2.0',
+    entry_points={
+        'console_scripts': [
+            'sbs_camera_node = mmlove_stereo_recorder.sbs_camera_node:main'
+        ],
+    },
 )
